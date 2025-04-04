7ca381381f749fb21730c72684bd04ca50a03c2a
Yogesh Maske <135599939+Itzmeyogesh@users.noreply.github.com>
2025-04-04 13:43:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn").addEventListener("click", function () {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+});
+import 'bootstrap/dist/css/bootstrap.min.css';
